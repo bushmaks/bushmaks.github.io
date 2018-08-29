335f62fda0c486fc0ff2b4a3351e657dcaf0eb2c
Adiela Abishua <adiela.abishua@gmail.com>
2018-08-29 17:40:56 +0300

    removed migrations from gitignore

--- socialreach/urls.py
+++ socialreach/urls.py
@@ -23,6 +23,4 @@ urlpatterns = [
     path('creators/', include('creators.urls')),
     path('', include('main_app.urls')),
     path('messages/', include('messages.urls')),
-    path('mpesa/', include('mpesa.urls'))
-    # path('mpesa/', include(('mpesa_api.core.urls', 'mpesa'), namespace='mpesa')),
 ]
